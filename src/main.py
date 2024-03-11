66029dba4ef725a217c6a5ae0a9e4565623af90a
Dmitry Skabin <skabin@list.ru>
2024-03-11 20:05:22 +0300

    model changed

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pickle
 import utils
 import pandas as pd
+import numpy as np
 
 PATH_TEST = "./data/test_df.csv"
 df_test = pd.read_csv(PATH_TEST)
@@ -8,6 +9,13 @@ df_test = pd.read_csv(PATH_TEST)
 with open("model.pkl", "rb") as f:
     clf = pickle.load(f)
 
-X_test, y_test = utils.get_training_samples(df_test)
+with open("scaler.pkl", "rb") as f:
+    scaler = pickle.load(f)
 
-utils.get_metrics(model=clf, X_test=X_test, y_test=y_test, verbose=False)
+X_test, y_test = utils.get_training_samples(df_test)
+X_test = scaler.transform(X_test)
+
+classes, probabilities, score = utils.get_metrics(model=clf, X_test=X_test, y_test=y_test, 
+                                                  verbose=False, return_values=True)
+
+np.savetxt("predicted_labels.csv", classes.astype(int), delimiter=",")
--- src/train_script.py
+++ src/train_script.py
@@ -1,28 +1,36 @@
 import pandas as pd
-import numpy as np
 import pickle
 
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 
 import utils
 
+
 PATH_TRAIN = "./data/train_df.csv"
 PATH_TEST  = "./data/test_df.csv"
-SAVE_MODEL = False
+SAVE_MODEL = True
 
 
 df_train = pd.read_csv(PATH_TRAIN)
 df_test  = pd.read_csv(PATH_TEST)
 
-X_train, y_train = utils.get_training_samples(df_train)
+X_train, y_train = utils.get_training_samples(df_train, synthetic=False)
 X_test, y_test = utils.get_training_samples(df_test)
 
-clf = LogisticRegression(max_iter=2000, class_weight="balanced")
-clf.fit(X_train, y_train)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
-utils.get_metrics(clf, X_test=X_test, y_test=y_test)
+clf = LogisticRegression(max_iter=2000, class_weight="balanced")
+clf.fit(X_train_scaled, y_train)
+
+utils.get_metrics(clf, X_test=X_test_scaled, y_test=y_test)
 
 ### SAVE MODEL
 if SAVE_MODEL:
     with open("model.pkl", "wb") as f:
         pickle.dump(clf, f)
+    
+    with open("scaler.pkl", "wb") as f:
+        pickle.dump(scaler, f)
